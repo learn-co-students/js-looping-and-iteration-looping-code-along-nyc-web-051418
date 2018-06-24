d2893e74861d07cd49af49290637142573067413
David Tenenbaum <github email address>
2018-06-24 19:41:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function printBadges(employees) {
+  for (let i = 0; i <  employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+  }
+  return employees;
+}
+
+function tailsNeverFails() {
+  let count = 0
+  function maybeTrue () {
+    return Math.random() >= 0.5;
+  }
+  while (maybeTrue()) {
+    count++;
+  }
+  return `You got ${count} tails in a row!`;
+}
