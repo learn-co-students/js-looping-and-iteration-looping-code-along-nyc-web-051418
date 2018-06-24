a74a91b49980d44840574744ab116b0979493cb4
JohnZBurnett <tuninator@gmail.com>
2018-06-24 18:30:22 -0400

    Finish

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function printBadges(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`)
+  }
+  return arr;
+}
+
+function tailsNeverFails() {
+  let flip = Math.random();
+  let numTails = 0;
+  console.log(flip);
+  while (flip >= 0.5) {
+    flip = Math.random();
+    numTails++;
+  }
+
+  return  `You got ${numTails} tails in a row!`;
+}
