d43b9622ec5ef3fafe2db8f0bffee780522bc059
github username <github email address>
2018-06-24 21:25:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 // Code your solutions in this file
-for (let age = 30; age < 40; age++) {
-  console.log(`I'm ${age} years old. Happy birthday to me!`);
-  // debugger
-} 
-
-const gifts = ['teddy bear', 'drone', 'doll'];
- 
-function wrapGifts (gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+function printBadges (names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
   }
- 
-  return gifts;
+
+  return names;
 }
- 
-wrapGifts(gifts);
+
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
+}
